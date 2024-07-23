59684e256a342d99771c3a1e9354dd8853a1d4e9
Kirill Konevets <kkonevec@qapp.tech>
2024-07-23 19:10:36 +0300

    first commit

---
+++ client.c
@@ -0,0 +1,36 @@
+#include "c_example_client.h"
+
+#include <erpc_transport_setup.h>
+#include <erpc_mbf_setup.h>
+#include <erpc_client_setup.h>
+
+
+void example_client(void)
+{
+    erpc_transport_t transport;
+    erpc_mbf_t message_buffer_factory;
+    erpc_client_t client_manager;
+
+    /* Init eRPC client infrastructure */
+    transport = erpc_transport_tcp_init("127.0.0.1", 5407, false);
+    message_buffer_factory = erpc_mbf_dynamic_init();
+    client_manager = erpc_client_init(transport, message_buffer_factory);
+
+    /* init eRPC client IO service */
+    initIO_client(client_manager);
+
+    // Now we can call the remote function to turn on the green LED.
+    set_led(kGreen, true);
+
+    /* deinit objects */
+    deinitIO_client();
+    erpc_client_deinit(client_manager);
+    erpc_mbf_dynamic_deinit(message_buffer_factory);
+    erpc_transport_tcp_deinit(transport);
+}
+
+int main()
+{
+    example_client();
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,44 @@
+#include "example_common.h"
+#include "c_example_server.h"
+
+#include "erpc_transport_setup.h"
+#include "erpc_mbf_setup.h"
+#include "erpc_server_setup.h"
+
+
+// Implement the remote function.
+void set_led(LEDName whichLed, bool onOrOff)
+{
+    // implementation goes here
+}
+
+void example_server(void)
+{
+    erpc_transport_t transport;
+    erpc_mbf_t message_buffer_factory;
+    erpc_server_t server;
+    erpc_service_t service = create_IO_service();
+
+    /* Init eRPC server infrastructure */
+    transport = erpc_transport_tcp_init("127.0.0.1", 5407, true);
+    message_buffer_factory = erpc_mbf_dynamic_init();
+    server = erpc_server_init(transport, message_buffer_factory);
+
+    /* add custom service implementation to the server */
+    erpc_add_service_to_server(server, service);
+
+    // Run the server.
+    erpc_server_run(server);
+
+    /* deinit objects */
+    destroy_IO_service(service);
+    erpc_server_deinit(server);
+    erpc_mbf_dynamic_deinit(message_buffer_factory);
+    erpc_transport_tcp_deinit(transport);
+}
+
+int main()
+{
+    example_server();
+    return 0;
+}
